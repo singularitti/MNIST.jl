4de0363d5417b661e3f519c1da69a85619fbcd75
singularitti <singularitti@outlook.com>
2023-04-17 04:06:11 -0400

    Add iteration interface for `Network`

--- src/types.jl
+++ src/types.jl
@@ -16,3 +16,28 @@ function Network(layers)
     return Network{length(layers)}(layers, weights, biases)
 end
 Network(layers::Integer...) = Network(layers)
+
+Base.iterate(network::Network) = (
+    (
+        first(network.layers),
+        zeros(size(first(network.weights))),
+        zeros(size(first(network.biases))),
+    ),
+    1,
+)
+function Base.iterate(network::Network, state)
+    if state >= length(network)
+        return nothing
+    else
+        return (
+            (network.layers[state], network.weights[state - 1], network.biases[state - 1]),  # Note the index here!
+            state + 1,
+        )
+    end
+end
+
+Base.eltype(::Network{N}) where {N} = (Int64, Matrix{Float64}, Vector{Float64})
+
+Base.length(::Network{N}) where {N} = N
+
+Base.size(network::Network) = size(network.layers)
