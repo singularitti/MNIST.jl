2f19d81a84830602dd9c28af9d1aa2378c733be8
singularitti <singularitti@outlook.com>
2023-04-23 21:11:19 -0400

    Fix `Estimator`

--- src/types.jl
+++ src/types.jl
@@ -45,7 +45,7 @@ end
 function (estimator::Estimator)(data::AbstractVector{Example})
     hits =
         sum(
-            argmax(estimator.network(estimator.f, example.x) - 1) == example.y for
+            argmax(estimator.network(estimator.f, example.x)) == argmax(example.y) for
             example in data
         ) / length(data)
     loss = mean(estimator.network(estimator.f, example) for example in data)
